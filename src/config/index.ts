0cda5f2f60eb77c61f475d69cf27e5c3414dea4e
AbabilCore <ababilspark@gmail.com>
2026-09-26 15:08:22 +0600

    fix(config): update production API URL to simplified domain

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 const API_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:8000"
-    : "https://xn--b5bqsp8e4a9a5gb5l.xn--94b0fay2dh.xn--54b7fta0cc";
+    : "https://envlink.ababilspark.com";
 
 export default API_URL;
