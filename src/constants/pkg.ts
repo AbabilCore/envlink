93e8ed52d28c5260fcc943ff8a7883a341dd8eb6
AbabilCore <ababilspark@gmail.com>
2026-09-26 15:12:27 +0600

    chore(release): bump version to 1.0.6

--- src/constants/pkg.ts
+++ src/constants/pkg.ts
@@ -6,7 +6,7 @@ interface PackageJson {
 
 const pkg: PackageJson = {
   name: "envlink",
-  version: "1.0.5",
+  version: "1.0.6",
   description: "Secure and anonymous environment file sharing CLI",
 };
 
